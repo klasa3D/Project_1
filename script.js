94fc92e3d9a44118edfb8376e8b01514d90f532a
filipk <filipk@gmail.com>
2023-10-16 14:54:14 +0200

    add img

---
+++ script.js
@@ -0,0 +1,11 @@
+const image = new Image();
+image.src = 'pilka.jpg';
+
+image.onclick = () => {
+    console.log(image);
+    
+};
+
+function funkcja1() {
+    console.log(document.getElementById("rys1"));
+}
