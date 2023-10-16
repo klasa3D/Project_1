f20bfa02e5eaeba4317c780e1a086a6d7a4f28ed
opium <opium@wp.pl>
2023-10-16 14:54:27 +0200

    Add image

---
+++ projekt2.js
@@ -0,0 +1,8 @@
+        function alercik(){
+            alert("Jasna dupa");
+        }
+
+        function ajaj(){
+            var body = document.body;
+            body.innerHTML = '<img src="MONKE.PNG">';
+        }
