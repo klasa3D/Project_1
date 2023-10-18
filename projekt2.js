8bd67411696152a85c0caab92672bc871f927fd3
Oskar <robak@gg.jp>
2023-10-18 12:19:44 +0200

    array

--- projekt2.js
+++ projekt2.js
@@ -6,3 +6,10 @@
             var body = document.body;
             body.innerHTML = '<img src="MONKE.PNG">';
         }
+        
+        function wypisz(){
+            let x=document.getElementById("num").value;
+            console.log(x);
+        }
+        let tab=[x,x,x,x];
+        console.log(tab+"<br>");
