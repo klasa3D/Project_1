bbffb4afe1aff93f66915639287829b3971a2c2e
Wiktor <Wiktor.gmail.com>
2023-10-27 12:20:25 +0200

    w

---
+++ write.js
@@ -0,0 +1,13 @@
+function showlog(id){
+    let txt = document.getElementById(id).value;
+    console.log(txt);
+    }
+    function showWrite(txt,id){
+        let txt1 =document.getElementById(id).value;
+        document.write(txt);
+    }
+    /*function pentla(start1,end2,coile3){
+        for( let i=start1;i<end2;i=i+coile3){
+
+        }
+    }*/
