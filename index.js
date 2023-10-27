4c930aa41492a4309d8c9fcaff1d19386f4f1a08
Roman <sf@gmail.com>
2023-10-27 15:58:03 +0200

    nowe

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-function f1(){
-    console log("f1");
+function petla(start1, end2, coile3){
+    console.log(start1, end2, coile3);
+    for (let i = start1; i<=end2; i=i+coile3){
+        console.log(i);
+    }
+    console.log("Koniec");
 }
-function f2(){
-    console log("f2");
+petla(0,10,1);
+
+function show(txt){
+    console.log(txt + ".");
 }
+
+function petla2(start1, end2, coile3){
+    show(start1, end2, coile3);
+    for (let i = start1; i>=end2; i=i-coile3){
+    show(i);
+    }
+    show("Koniec");
+}
+petla(0,10,1);
